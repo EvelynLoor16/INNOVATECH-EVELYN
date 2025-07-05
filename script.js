e26b1cc29d1e17b4005b12208d1dd9854608f1d4
EvelynLoor16 <evelynloor05@gmail.com>
2025-07-04 22:45:19 -0500

    cambios8

--- script.js
+++ script.js
@@ -68,6 +68,7 @@ document.getElementById('payButton').addEventListener('click', () => {
     return;
   }
 
+  // Calcular total en centavos
   const total = cart.reduce((sum, item) => sum + item.price, 0);
   const totalCentavos = Math.round(total * 100);  // Convertir a centavos
   const baseSinIVA = Math.round(totalCentavos / 1.11);  // Monto sin IVA
@@ -90,6 +91,7 @@ document.getElementById('payButton').addEventListener('click', () => {
   const button = PayPhone.Button({
     token: "OIUirdECIEZuryUo288QjF61WMxr-PUi1lHAwicDRFsYdk73OME5QslNeTC-zGyZY4KdedeGFtgzqDFQe5KGqiHqAc-2kMMf5xSDRERDcz_5tC46-9BwJmn6UU_57mSCN07wE_inzwNN895r7cNfg1E9PYNBSHznz51_q8mWK0x9lXPq1msuBowat1a2MWSNC2qtBWvvWI_YiLeKPCDy2fd33i8A7dXFszokNkxK7YAXgP7lesqP3hogmdMLZLwXPYYv-xI5cANlXu6thdU3_wbqiSuBFqp6cjxwevz-74nbz7mT0BB184oBnC6Vzo-QOuvqR9bD9oGMhfnCGBoGaRJmEiY",  // Reemplaza con tu token de PayPhone
     btnText: "Pagar ahora",  // Texto del botón
+     storeId: "fedbba10-1954-4e89-a11a-33a2b04fd2c0", // ID de la tienda
     createOrder: () => paymentRequest, // Detalles de la orden
     container: "payphone-button-container", // Contenedor donde se renderiza el botón
     onComplete: function (response) {
@@ -105,7 +107,9 @@ document.getElementById('payButton').addEventListener('click', () => {
   });
 
   payphoneContainer.appendChild(button);  // Agregar el botón al contenedor
+ 
 });
+
 
 
 
